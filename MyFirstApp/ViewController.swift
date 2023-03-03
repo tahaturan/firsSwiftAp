f2986c2ae076d969a29d73a02e23cb73becb319b
tahaturan <tahaaturann16@gmail.com>
2023-03-03 06:59:47 +0300

    ilk swift uygulamam

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    var image1 = "harry-potter-oyunculari-hogwarts-a-donuyor-944903-5"
+    var image2 = "harry-potter"
+    var change = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeButton(_ sender: Any) {
+        if change {
+            imageView.image = UIImage(named: image1)
+        } else {
+            imageView.image = UIImage(named: image2)
+        }
+        change = !change
+    }
+    
 }
 
